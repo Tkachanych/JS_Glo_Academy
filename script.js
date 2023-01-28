135cd84851693ccf17dad8c91017ddcee4bfb07c
Vladimir Tkachenko <geletar@gmail.com>
2023-01-28 12:23:09 +0400

    Первое задание

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-"use strict";
+'use strict';
 
-let title = 'Test';
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'JS_Glo_Academy';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 2500;
+let rollback = 15;
+let fullPrice = 500000;
+let adaptive = true;
 
-alert(title);
-console.log('Текст в консоли');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(screens.toLowerCase().split(', '));
+console.log(fullPrice * (rollback / 100));
