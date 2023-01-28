a5cb9c7b1d1044c94600bcb81f0c9cd54a91b0f3
Vladimir Tkachenko <geletar@gmail.com>
2023-01-28 11:53:36 +0400

    Исправление замечаний.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var title = 'Test';
-var screens;
-var screenPrice;
-var rollback;
-var fullPrice;
-var adaptive;
+"use strict";
+
+let title = 'Test';
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
 
 alert(title);
 console.log('Текст в консоли');
