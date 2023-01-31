6a463b2edb22df7281aabda957f29229cf594a3b
Vladimir Tkachenko <81316027+Tkachanych@users.noreply.github.com>
2023-01-31 21:28:07 +0400

    lesson03 (#3)
    lesson03

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 'use strict';
 
-let title = 'JS_Glo_Academy';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 2500;
-let rollback = 15;
-let fullPrice = 500000;
-let adaptive = true;
+const title = prompt('Как называется Ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?','Простые, Сложные, Интерактивные');
+const screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?'));
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = parseFloat(prompt('Сколько это будет стоить?'));
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = parseFloat(prompt('Сколько это будет стоить?'));
 
+const rollback = 15;
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+
+//Расчёт скидки.
+const discount = function(price) {
+  if (price > 30000)
+    return 'Даем скидку в 10%';
+  if (price > 15000)
+    return 'Даем скидку в 5%';
+  if (price >= 0)
+    return 'Скидка не предусмотрена';
+  return 'Что-то пошло не так.';
+};
+
+//Весь функционал, что был ранее оставляем
+console.log('servicePercentPrice = ' + servicePercentPrice);
+console.log(discount(fullPrice));
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
