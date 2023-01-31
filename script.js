e526569aaefe7501febad7dee3606a456dd94630
Vladimir Tkachenko <geletar@gmail.com>
2023-01-31 22:02:37 +0400

    lesson04

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
 'use strict';
 
-const title = prompt('Как называется Ваш проект?');
-const screens = prompt('Какие типы экранов нужно разработать?','Простые, Сложные, Интерактивные');
-const screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?'));
-const adaptive = confirm('Нужен ли адаптив на сайте?');
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice1 = parseFloat(prompt('Сколько это будет стоить?'));
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice2 = parseFloat(prompt('Сколько это будет стоить?'));
+let title = prompt('Как называется Ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?','Простые, Сложные, Интерактивные');
+let screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?'));
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = parseFloat(prompt('Сколько это будет стоить?'));
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = parseFloat(prompt('Сколько это будет стоить?'));
+let rollback = 15;
 
-const rollback = 15;
+const showTypeOf = function(variable) {
+  console.log(variable, typeof variable);
+}
 
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+//Функция возвращает сумму всех дополнительных услуг.
+const getAllServicePrices = function(price1, price2) {
+  return price1 + price2;
+}
+
+//Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
+function getFullPrice(price1, price2) {
+  return price1 + price2;
+}
 
 //Расчёт скидки.
-const discount = function(price) {
-  if (price > 30000)
-    return 'Даем скидку в 10%';
-  if (price > 15000)
-    return 'Даем скидку в 5%';
-  if (price >= 0)
-    return 'Скидка не предусмотрена';
+const getRollbackMessage = function(price) {
+  if (price > 30000) return 'Даем скидку в 10%';
+  if (price > 15000) return 'Даем скидку в 5%';
+  if (price >= 0) return 'Скидка не предусмотрена';
   return 'Что-то пошло не так.';
-};
+}
 
-//Весь функционал, что был ранее оставляем
-console.log('servicePercentPrice = ' + servicePercentPrice);
-console.log(discount(fullPrice));
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
+//Функция возвращает строку: первый символ с большой буквы, остальные с маленькой.
+const getTitle = function(string) {
+  let str = string.trim().toLowerCase();
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+//Функция возвращает итоговую стоимость за вычетом процента отката
+const getServicePercentPrices = function(price, rollback) {
+  return Math.ceil(price - (price * (rollback / 100)));
+}
+
+const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
+const servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
 console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
 console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
 console.log(screens.toLowerCase().split(', '));
-console.log(fullPrice * (rollback / 100));
+console.log(servicePercentPrice);
+console.log(getTitle(title));
+console.log(getRollbackMessage(fullPrice));
