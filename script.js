2047b7eaac13d1c35342b5b6bf98b9ef5b961d7e
Vladimir Tkachenko <geletar@gmail.com>
2023-02-03 22:01:33 +0400

    lesson07

--- script.js
+++ script.js
@@ -1,51 +1,85 @@
 'use strict';
 
-const game = function () {
+const appData = {
 
-  let userAnswer;
+  title: '',
+  screens: '',
+  screenPrice: 0,
+  adaptive: true,
+  rollback: 15,
+  rollbackMessage: '',
+  allServicePrices: 0,
+  service1: '',
+  service2: '',
+  fullPrice: 0,
+  servicePercentPrice: 0,
 
-  const numToGuess = Math.floor(Math.random() * 100) + 1;
-  console.log(numToGuess);
+  isNumber: function (num) {
+    return isNaN(num);
+  },
 
-  const isNumber = function (num) {
-    return typeof num === 'number' && isFinite(num);
-  }
+  asking: function () {
+    this.title = prompt('Как называется Ваш проект?', 'Калькулятор вёрстки');
+    this.screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 
-  const getAnswer = function (str) {
-    if (str === null) {
-      return null;
-    } else if (isNumber(+str) && str !== '') {
-      return parseInt(str);
-    } else {
-      return getAnswer(prompt('Введи число!'));
+    do {
+      this.screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?'));
+    } while (this.isNumber(this.screenPrice));
+
+    this.adaptive = confirm('Нужен ли адаптив на сайте?');
+  },
+
+  getTitle: function (string) {
+    let str = string.trim().toLowerCase();
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  },
+
+  getAllServicePrices: function () {
+    let servicePrice = 0;
+
+    for (let i = 0; i < 2; i++) {
+      if (i === 0) {
+        this.service1 = prompt('Какой дополнительный тип услуги нужен?');
+      } else if (i === 1) {
+        this.service2 = prompt('Какой дополнительный тип услуги нужен?');
+      }
+      do {
+        servicePrice = parseFloat(prompt('Сколько это будет стоить?', 0));
+      } while (this.isNumber(servicePrice));
+      this.allServicePrices += servicePrice;
     }
-  }
+  },
 
-  const tryToGues = function (numToGuess) {
+  getFullPrice: function () {
+    this.fullPrice = this.screenPrice + this.allServicePrices;
+  },
 
-    userAnswer = getAnswer(prompt('Угадай число от 1 до 100'));
+  getRollbackMessage: function (price) {
+    if (price > 30000) this.rollbackMessage = 'Даем скидку в 10%';
+    else if (price > 15000) this.rollbackMessage = 'Даем скидку в 5%';
+    else if (price >= 0) this.rollbackMessage = 'Скидка не предусмотрена';
+    else this.rollbackMessage = 'Что-то пошло не так.';
+  },
 
-    switch (true) {
+  getServicePercentPrices: function () {
+    this.servicePercentPrice = Math.ceil(this.fullPrice - (this.fullPrice * (this.rollback / 100)));
+  },
 
-      case userAnswer === null:
-        alert('Игра окончена!');
-        return;
-
-      case numToGuess < userAnswer:
-        alert('Загаданное число меньше');
-        return tryToGues(numToGuess);
-
-      case numToGuess > userAnswer:
-        alert('Загаданное число больше');
-        return tryToGues(numToGuess);
-
-      case numToGuess === userAnswer:
-        alert('Поздравляю, Вы угадали!!!');
-        return;
+  logger: function () {
+    for (let key in this) {
+      console.log(this[key]);
     }
-  }
+  },
 
-  tryToGues(numToGuess);
+  start: function () {
+    this.asking();
+    this.title = this.getTitle(this.title);
+    this.getAllServicePrices();
+    this.getFullPrice();
+    this.getRollbackMessage(this.fullPrice);
+    this.getServicePercentPrices();
+    this.logger();
+  },
 }
 
-game();
+appData.start();
