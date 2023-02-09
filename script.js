497cb1b4dc248ab69ba9ffe3cad25b3a7dbbcdc5
Vladimir Tkachenko <geletar@gmail.com>
2023-02-09 15:16:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 'use strict'
 
+const button = document.getElementById('btn');
+const input = document.getElementById('text');
+const square = document.getElementById('square');
+
+const setSqareColor = function() {
+  square.setAttribute(bac)
+}
+
+console.dir(square);
+
+//button.addEventListener('click')
