88e4ca67ef9d543303a444027f686efb7494530a
Vladimir Tkachenko <geletar@gmail.com>
2023-02-10 08:23:41 +0400

    Исправление замечаний

--- script.js
+++ script.js
@@ -8,18 +8,24 @@ const circleButton = document.getElementById('e_btn');
 const inputRange = document.getElementById('range');
 const rangeSpan = document.getElementById('range-span');
 
+const circleStartWidth = circle.offsetWidth;
+const circleStartHeight = circle.offsetHeight;
+
 const setSqareColor = function() {
   if (inputText.value.trim() != '') {
     square.style.backgroundColor = inputText.value;
   }
-  input.value = '';
+  inputText.value = '';
 }
 
 const setCircleSize = function (event) {
-  rangeSpan.textContent = event.target.value;
-  circle.style.width = event.target.value + '%';
-  circle.style.height = event.target.value + '%';
+  rangeSpan.textContent = event.target.value + '%';
+  circle.style.height = (circleStartHeight * event.target.value/100) + 'px';
+  circle.style.width = (circleStartWidth * event.target.value/100) + 'px';
 }
+
+inputRange.value = '100';
+rangeSpan.textContent = '100%';
 
 circleButton.style.display = 'none';
 
