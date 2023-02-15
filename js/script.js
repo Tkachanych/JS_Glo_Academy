9057be7769253b771230fd36a4245919dacbfa58
Vladimir Tkachenko <geletar@gmail.com>
2023-02-15 23:33:27 +0400

    lesson15

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const DomElement = function (selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+
+}
+
+DomElement.prototype.createElement = function () {
+  switch (this.selector[0]) {
+    case '.':
+      console.log('<div> .class');
+      break;
+      case '#':
+      console.log('<p> #id');
+      break;
+  }
+}
+
+const newElem = new DomElement('.block', 10,10,'red',15);
+
+newElem.createElement();
