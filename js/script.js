2a28ca85608bf12cbd73038d76b0cc7bd065670c
Vladimir Tkachenko <geletar@gmail.com>
2023-02-17 00:33:01 +0400

    Исправление замечаний.

--- js/script.js
+++ js/script.js
@@ -12,51 +12,38 @@ const DomElement = function (selector, height, width, bg, fontSize) {
 }
 
 DomElement.prototype.createElement = function (text) {
-
-  const createDiv = () => {
-    const newDiv = document.createElement("div");
-
-    newDiv.classList.add(this.selector.slice(1))
-    newDiv.style.height = this.height + 'px';
-    newDiv.style.width = this.width + 'px';
-    newDiv.style.fontSize = this.fontSize + 'px';
-    newDiv.style.background = this.bg;
-    newDiv.innerText = text;
-
-    document.body.append(newDiv);
-  }
-
-  const createPar = () => {
-    const newPar = document.createElement("p");
-
-    newPar.setAttribute('id', this.selector.slice(1))
-    newPar.style.height = this.height + 'px';
-    newPar.style.width = this.width + 'px';
-    newPar.style.fontSize = this.fontSize + 'px';
-    newPar.style.background = this.bg;
-    newPar.innerText = text;
-
-    document.body.append(newPar);
-  }
+  let prop;
+  let elem;
 
   switch (this.selector[0]) {
     case '.':
-      createDiv();
+      elem = 'div';
+      prop = 'class';
       break;
     case '#':
-      createPar();
+      elem = 'p';
+      prop = 'id';
       break;
   }
+
+  const newElem = document.createElement(elem);
+
+  newElem.setAttribute(prop, this.selector.slice(1));
+  newElem.style.height = this.height + 'px';
+  newElem.style.width = this.width + 'px';
+  newElem.style.fontSize = this.fontSize + 'px';
+  newElem.style.background = this.bg;
+  newElem.innerText = text;
+
+  document.body.append(newElem);
 }
 
-const divElem1 = new DomElement('.block', 15, 100, 'red', 15);
+const divElem1 = new DomElement('.block', 50, 100, 'red', 15);
 const divElem2 = new DomElement('.block2', 20, 100, 'blue', 18);
 const divElem3 = new DomElement('.block3', 20, 100, 'purple', 15);
 const pElem1 = new DomElement('#best1', 30, 150, 'green', 20);
 const pElem2 = new DomElement('#best2', 30, 150, 'grey', 20);
-const pElem3 = new DomElement('#best3', 30, 150, 'yellow', 20);
-
-
+const pElem3 = new DomElement('#best3', 50, 250, 'yellow', 30);
 
 divElem1.createElement(textForDiv);
 divElem2.createElement(textForDiv);
